2dcb659fb7881472c5f06604d28e90ab0de6706b
Sandri Robin <robinsandri@gmail.com>
2026-04-08 17:37:19 +0200

    Question6

---
+++ src/Telecommande.java
@@ -0,0 +1,27 @@
+public class Telecommande {
+
+    public Telecommande() {
+        throw new Error("code non ecrit");
+    }
+
+    public void ajouterLampe(Lampe lampe) {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerLampe(int indiceLampe) {
+        throw new Error("code non ecrit");
+    }
+
+    public void desactiverLampe(int indiceLampe) {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerTout() {
+        throw new Error("code non ecrit");
+    }
+
+    @Override
+    public String toString() {
+        throw new Error("code non ecrit");
+    }
+}
