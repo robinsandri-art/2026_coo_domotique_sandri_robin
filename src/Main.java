b5332725f8c57208150e8121a2c2c864e7ade22f
Sandri Robin <robinsandri@gmail.com>
2026-04-09 15:43:39 +0200

    Questions10TP5

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,61 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- * classe qui represente une telecommande pilotant plusieurs lampes
- */
-public class Telecommande {
+public class Main {
 
-    /**
-     * liste des lampes controlees
-     */
-    private ArrayList<Lampe> lampes;
+    public static void main(String args[]) {
+        Telecommande t = new Telecommande();
 
-    /**
-     * construit une telecommande vide
-     */
-    public Telecommande() {
-        this.lampes = new ArrayList<Lampe>();
+        ajouterAppareils(t);
+
+        t.activer(1);
+        t.activer(3);
+        System.out.println(t);
+
+        lancerMenu(t);
     }
 
-    /**
-     * ajoute une lampe a la telecommande
-     *
-     * @param lampe lampe a ajouter
-     */
-    public void ajouterLampe(Lampe lampe) {
-        this.lampes.add(lampe);
-    }
+    private static void lancerMenu(Telecommande t) {
+        System.out.println(t);
+        Scanner sc = new Scanner(System.in);
 
-    /**
-     * active la lampe a l'indice donne
-     *
-     * @param indiceLampe indice de la lampe
-     */
-    public void activerLampe(int indiceLampe) {
-        this.lampes.get(indiceLampe).allumer();
-    }
+        boolean fini = false;
 
-    /**
-     * desactive la lampe a l'indice donne
-     *
-     * @param indiceLampe indice de la lampe
-     */
-    public void desactiverLampe(int indiceLampe) {
-        this.lampes.get(indiceLampe).eteindre();
-    }
+        while (!fini) {
+            System.out.println("entrer le numero de l'appareil");
+            int choix = sc.nextInt();
+            System.out.println("entrer commande (+/-/exit)");
+            String com = sc.nextLine();
+            com = sc.nextLine();
 
-    /**
-     * active toutes les lampes
-     */
-    public void activerTout() {
-        for (Lampe l : this.lampes) {
-            l.allumer();
+            if (com.equals("+")) {
+                System.out.println("== activer " + choix + " ==");
+                t.activer(choix);
+            } else if (com.equals("-")) {
+                System.out.println("== desactiver " + choix + " ==");
+                t.desactiver(choix);
+            } else if (com.equals("exit")) {
+                System.out.println("== Fin du programme == ");
+                fini = true;
+            } else {
+                System.out.println("commande inconnue");
+            }
+
+            System.out.println(t);
         }
+        sc.close();
     }
 
-    /**
-     * retourne la description de la telecommande
-     *
-     * chaque lampe est precedee de son indice
-     */
-    @Override
-    public String toString() {
-        String r = "";
-        for (int i = 0; i < this.lampes.size(); i++) {
-            r += i + " " + this.lampes.get(i) + "\n";
-        }
-        return r;
+    private static void ajouterAppareils(Telecommande t) {
+        Lampe l1 = new Lampe("Lampe1");
+        t.ajouterAppareil(l1);
+
+        Lampe l2 = new Lampe("Lampe2");
+        t.ajouterAppareil(l2);
+
+        Hifi h1 = new Hifi();
+        t.ajouterAppareil(h1);
+
+        Lampe l3 = new Lampe("Lampe3");
+        t.ajouterAppareil(l3);
     }
 }
